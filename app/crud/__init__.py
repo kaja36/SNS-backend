b0d7281e29f1778ad0568008f8654bb315d7e5e2
kaja36 <2412110185a@kindai.ac.jp>
2026-02-02 23:16:07 +0900

    change: 名前空間を新たに設定

---
+++ app/crud/__init__.py
@@ -0,0 +1,18 @@
+from crud.users import *
+from crud.posts import *
+
+__all__ = [
+    "create_user",
+    "get_all_users",
+    "get_user_by_id",
+    "get_user_by_username",
+    "update_user",
+    "delete_user",
+    "prove_user_exists",
+    "create_post",
+    "get_all_posts",
+    "get_post_by_id",
+    "get_posts_by_user_id",
+    "update_post",
+    "delete_post",
+]
